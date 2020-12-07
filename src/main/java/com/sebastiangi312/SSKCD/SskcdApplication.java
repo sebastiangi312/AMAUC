dea9cb763fd96c7b81cad7ab268a083c89e7bc21
Sebastian Giraldo <40746232+sebastiangi312@users.noreply.github.com>
2020-12-06 19:11:34 -0500

    The baisc strucutre of the project has been created

---
+++ src/main/java/com/sebastiangi312/SSKCD/SskcdApplication.java
@@ -0,0 +1,13 @@
+package com.sebastiangi312.SSKCD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SskcdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SskcdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sebastiangi312/SSKCD/SskcdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sebastiangi312.SSKCD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SskcdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
