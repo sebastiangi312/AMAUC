45fea7b4d1295c4822f4a40a3583ee8f2381fc9a
Sebastian Giraldo <40746232+sebastiangi312@users.noreply.github.com>
2020-12-20 16:11:22 -0500

    Added Tests

--- src/test/java/com/sebastiangi312/SSKCD/handlersTest/PersistanceCourseHandlerTest.java
+++ src/test/java/com/sebastiangi312/SSKCD/handlersTest/PersistanceCourseHandlerTest.java
@@ -4,23 +4,25 @@ import com.sebastiangi312.SSKCD.application.handler.PersistanceCourseHandler;
 import com.sebastiangi312.SSKCD.persistance.CourseRepositoryService;
 import org.junit.Assert;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.transaction.Transactional;
+
 @SpringBootTest
+@Transactional
 public class PersistanceCourseHandlerTest {
   
-  private final PersistanceCourseHandler courseHandler;
-  private final CourseRepositoryService courseRepositoryService;
+  @Autowired
+  private PersistanceCourseHandler persistanceCourseHandler;
   
-  public PersistanceCourseHandlerTest(PersistanceCourseHandler courseHandler,
-                                      CourseRepositoryService courseRepositoryService) {
-    this.courseHandler = courseHandler;
-    this.courseRepositoryService = courseRepositoryService;
-  }
+  @Autowired
+  private CourseRepositoryService courseRepositoryService;
+  
   
   @Test
   public void should_save_courses(){
-    courseHandler.saveCourses("BASE DE DATOS II (3007848)","3");
+    persistanceCourseHandler.saveCourses("BASE DE DATOS II (3007848)","3");
   
     Assert.assertEquals("3007848", courseRepositoryService.getCourse("3007848").getCode());
   }
